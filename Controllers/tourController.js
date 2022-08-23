111059e8047d5581d48a8969130f1bee6730a3e7
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-08-23 17:14:28 +0530

    In Progress (❁´◡❁)

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -76,7 +76,7 @@ exports.createTour = async (req, res) => {
     } catch (error) {
       res.status(400).json({
         status: 'fail',
-        message: 'Invalid data sent!'
+        message: error
       })
     }
 };
--- Models/tourModel.js
+++ Models/tourModel.js
@@ -13,7 +13,7 @@ const tourSchema = new mongoose.Schema(
       minlength: [10, 'A tour name must have more or equal then 10 characters']
       // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
-    slug: String,
+    // slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -25,16 +25,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
-      enum: {
-        values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult'
-      }
+    //   enum: {
+    //     values: ['easy', 'medium', 'difficult'],
+    //     message: 'Difficulty is either: easy, medium, difficult'
+    //   }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [1, 'Rating must be above 1.0'],
-      max: [5, 'Rating must be below 5.0']
+    //   min: [1, 'Rating must be above 1.0'],
+    //   max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -46,13 +46,13 @@ const tourSchema = new mongoose.Schema(
     },
     priceDiscount: {
       type: Number,
-      validate: {
-        validator: function(val) {
-          // this only points to current doc on NEW document creation
-          return val < this.price;
-        },
-        message: 'Discount price ({VALUE}) should be below regular price'
-      }
+    //   validate: {
+    //     validator: function(val) {
+    //       // this only points to current doc on NEW document creation
+    //       return val < this.price;
+    //     },
+    //     message: 'Discount price ({VALUE}) should be below regular price'
+    //   }
     },
     summary: {
       type: String,
@@ -71,23 +71,25 @@ const tourSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now(),
-      select: false
+    //   select: false
     },
     startDates: [Date],
-    secretTour: {
-      type: Boolean,
-      default: false
-    }
+    // secretTour: {
+    //   type: Boolean,
+    //   default: false
+    // }
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  }
+//   {
+//     toJSON: { virtuals: true },
+//     toObject: { virtuals: true }
+//   }
 );
+
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
@@ -105,6 +107,7 @@ tourSchema.pre('save', function(next) {
 //   next();
 // });
 
+
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
@@ -118,6 +121,7 @@ tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
+
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
