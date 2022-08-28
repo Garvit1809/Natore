d4a223a31b99c5ad6a377e7350bfa1d129892a19
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-08-28 19:17:37 +0530

    TourModel Update

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-// const validator = require('validator');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,10 +10,10 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters']
-      // validate: [validator.isAlpha, 'Tour name must only contain characters']
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+      validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
-    // slug: String,
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -25,16 +25,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
-    //   enum: {
-    //     values: ['easy', 'medium', 'difficult'],
-    //     message: 'Difficulty is either: easy, medium, difficult'
-    //   }
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
-    //   min: [1, 'Rating must be above 1.0'],
-    //   max: [5, 'Rating must be below 5.0']
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -46,13 +46,13 @@ const tourSchema = new mongoose.Schema(
     },
     priceDiscount: {
       type: Number,
-    //   validate: {
-    //     validator: function(val) {
-    //       // this only points to current doc on NEW document creation
-    //       return val < this.price;
-    //     },
-    //     message: 'Discount price ({VALUE}) should be below regular price'
-    //   }
+      validate: {
+        validator: function(val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
     },
     summary: {
       type: String,
@@ -74,15 +74,15 @@ const tourSchema = new mongoose.Schema(
       select: false // can exclude fields right from model
     },
     startDates: [Date],
-    // secretTour: {
-    //   type: Boolean,
-    //   default: false
-    // }
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
-//   {
-//     toJSON: { virtuals: true },
-//     toObject: { virtuals: true }
-//   }
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
 
