10c8ade300a66b13badc1ba9013ea1fbe05ce54f
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-09-08 00:51:54 +0530

    JWT Token Setup

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,28 +1,22 @@
 const User = require('../Models/userModel');
 const catchAsync = require('./../Utils/catchAsync')
+const jwt = require('jsonwebtoken')
 
-// exports.signup = async (req,res,next) => {
-//     try {
-//         const newUser = await User.create(req.body);
-
-//     res.status(201).json({
-//         status: 'success',
-//         data: {
-//             user: newUser
-//         }
-//     })
-//     } catch (error) {
-//         res.status(400).json({
-//             status: 'fail',
-//             error
-//         })
-//     }
-// }
 exports.signup = catchAsync(async (req,res,next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+      });
+
+      const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+      })
 
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
