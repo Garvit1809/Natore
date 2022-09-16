1a853e0a184307cb0d6f916753374c770e30793b
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-09-16 12:35:05 +0530

    GetAll Factory Handler

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -25,19 +25,6 @@ exports.checkPassword = (req,res,next) => {
 }
 
 
-exports.getAllUsers = catchAsync(async (req, res) => {
-  const users = await User.find();
-
-  res.status(200).json({
-    status: "success",
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user tries to change password
   checkPassword()
@@ -67,6 +54,7 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User)
 exports.updateUser = factory.updateOne(User)
 exports.deleteUser = factory.deleteOne(User) 
--- Utils/apiFeatures.js
+++ Utils/apiFeatures.js
@@ -47,7 +47,7 @@ class APIFeatures {
       //  page=2&limit=10, 1-10 => page1, 11-20 => page2, ....
       //  query = query.skip(10).limit(10) => for Page2
       const page = this.queryString.page * 1 || 1;
-      const limit = this.queryString.limit * 1 || 9;
+      const limit = this.queryString.limit * 1 || 10;
       const skipBy = (page - 1) * limit;
   
       this.query = this.query.skip(skipBy).limit(limit);
