5f41314956c9ce24ac7d475064b0adec4ab1af2c
Garvit1809 <89806031+Garvit1809@users.noreply.github.com>
2022-09-18 19:16:04 +0530

    Tour Template Added

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewController = require('../Controllers/viewController')
 const router = express.Router();
 
 router.get("/", viewController.getOverview)
-router.get("/tour", viewController.getTour)
+router.get("/tour/:slug", viewController.getTour)
 
 module.exports = router;
